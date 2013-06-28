e9b068a2c6ba757ad29f402f6e47a5dd543d0bfe
Scott <scott.tolksdorf@gmail.com>
2013-06-27 20:55:18 -0400

    Created the basic flip animation

--- demo/index.js
+++ demo/index.js
@@ -20,28 +20,102 @@ $(document).ready(function(){
 
 	Parallax.transitions.flipRight = function(newPage, currentPage, viewPort, options, finished){
 
-		newPage.addClass('flippant-back');
-		currentPage.addClass('flippant');
+		//add the parallax css classes
+		//add the custom transition time property
 
-		viewPort.addClass('flipper');
+		newPage.addClass('parallax-flip-back').css({
+			left : 0,
+			top : 0,
+			'-webkit-transition' : 'all ' + options.animation_time + 'ms ease'
+		}).show();
 
-		currentPage.addClass('flipped');
+		currentPage.addClass('parallax-flip-front').css({
+			'-webkit-transition' : 'all ' + options.animation_time + 'ms ease'
+		});
 
 
-		console.log('flippeing');
+		setTimeout(function(){
+			newPage.addClass('flipRight');
+			currentPage.addClass('flipRight');
+			setTimeout(function(){
+				newPage.removeClass('parallax-flip-back').removeClass('flipRight').css('-webkit-transition', 'all 0 ease 0');
+				currentPage.removeClass('parallax-flip-front').removeClass('flipRight').css('-webkit-transition', 'all 0 ease 0').hide();
+				finished();
+				console.log('flippeing');
+			}, options.animation_time);
+		}, 0);
 
-		finished();
+	};
 
 
+	Parallax.transitions.flipRight = function(newPage, currentPage, viewPort, options, finished){
+		newPage
+			.cssAnimate({
+				'transform':'rotateY(-180deg)',
+				'backface-visibility' :'hidden'
+			},0)
+			.css({
+				left : 0,
+				top : 0,
+			}).show();
+
+		currentPage
+			.cssAnimate({'backface-visibility' :'hidden'},0, function(){
+				currentPage.cssAnimate({'transform':'rotateY(-180deg)'},options.animation_time);
+				newPage.cssAnimate({'transform':'rotateY(0deg)'},options.animation_time, function(){
+					currentPage.hide().cssAnimate({'transform':'rotateY(0deg)'},0);
+					finished();
+				});
+			});
 	};
 
 
 
 
-	viewPort = $('.container').parallax({
+	jQuery.fn.cssAnimate = function(rules, p2, p3){
+		var obj = $(this), delay = 400, callback;
+		if(typeof rules !== 'object') return this;
+		if(typeof p2 === 'number') delay = p2;
+		if(typeof p2 === 'function') callback = p2;
+		if(typeof p3 === 'function') callback = p3;
+
+		var vendors = ['', '-moz-', '-ms-', '-webkit-'];
+
+		var addRule = function(ruleName, rule){
+			$.each(vendors, function(index, vendor){
+				obj.css(vendor + ruleName, rule);
+			});
+		};
+		setTimeout(function(){
+			addRule('transition', 'all ' + delay + 'ms ease 0s');
+			$.each(rules, function(ruleName, rule){
+				addRule(ruleName, rule);
+			});
+		},0);
+		setTimeout(function(){
+			addRule('transition', 'all 0s ease 0s');
+			if(typeof callback === 'function') callback();
+		},delay);
+
+		return obj;
+	}
+
+
+
+
+
+
+
+
+
+
+
+	viewPort = $('.container2').parallax({
 		animation_time : 800,
 		parallax_scale : 0.3
 	});
+
+
 
 /*
 	setInterval( function(){
@@ -50,11 +124,12 @@ $(document).ready(function(){
 */
 
 
-$('.container').mouseover(function(){
+$('.container2').mouseover(function(){
 	console.log('asdsad');
-	viewPort.next().flipRight();
+	//viewPort.next().right();
 });
 
+//viewPort.next().flipRight();
 
 });
 
